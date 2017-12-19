f8b42570758c610f2f7cdb66f7d5df86516c49dc
MaySoMusician <maysomusician@gmail.com>
2017-12-19 23:28:19 +0900

    Balance応答の正規表現を修正
    ニックネームに対応したはず

--- events/message.js
+++ events/message.js
@@ -8,7 +8,7 @@ module.exports = (XPBot, message) => {
 
   if(message.author.bot){
     if(XPBot.config.mainBots.includes(message.author.id)){
-      let regBalanceMsg = /<@(\d+)>,\sBalance:\s\d+(?:\.\d*)?(?:[eE][+-]?\d+)?\s-\s(X\w*)/;
+      let regBalanceMsg = /<@!?(\d+)>,\sBalance:\s\d+(?:\.\d*)?(?:[eE][+-]?\d+)?\s-\s(X\w*)/;
       let regInfo = regBalanceMsg.exec(message.content);
 
       if(regInfo){
