57135c0231a78bcb01e0ec64291883b84817fb58
MaySoMusician <maysomusician@gmail.com>
2017-12-26 01:23:39 +0900

    オンラインになったときに反応するように変更

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -12,5 +12,21 @@ module.exports = (XPBot, oldUser, newUser) => {
 
     }
   } else{
+    //console.log(oldUser.id);
+    if(newUser.id == '352230801741578240' && newUser.presence.status == 'online') {//Kazmaro
+      var guild = XPBot.guilds.get(newUser.guild.id);
+      var chat2 = guild.channels.find('name', 'chat_2');
+      if(chat2){
+        chat2.send('<@352230801741578240> 嬢 おはよ！わよ！');
+      }
+      
+    }
+    /*if(newUser.id == '353169534984912896' && newUser.presence.status == 'online') {
+      var guild = XPBot.guilds.get(newUser.guild.id);
+      var chat2 = guild.channels.find('name', 'bot_control');
+      if(chat2){
+        chat2.send('<@353169534984912896> 嬢 おはよ！わよ！');
+      }
+    }*/
   }
 };
