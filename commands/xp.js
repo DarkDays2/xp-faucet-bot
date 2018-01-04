5f2253ecfa83f9a08b6018262d7d5c3c63e39a7f
MaySoMusician <maysomusician@gmail.com>
2018-01-04 15:41:20 +0900

    XPJPWallet案内用コマンド実装

---
+++ commands/xp.js
@@ -0,0 +1,21 @@
+exports.run = async (XPBot, message, args, level) => { // eslint-disable-line no-unused-vars
+  message.reply('XPJPWalletのコマンドは `!xp` です。よく確認してもう一度お試しください。')
+  .then(async msg => {
+    await XPBot.wait(20000);
+    msg.delete();
+  });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "一般利用者"
+};
+
+exports.help = {
+  name: "xp",
+  category: "利用不可",
+  description: "利用不可",
+  usage: "xp"
+};
