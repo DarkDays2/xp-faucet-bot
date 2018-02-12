d822690b571c0f91732debb71061dcd9656a1a58
MaySoMusician <maysomusician@gmail.com>
2018-02-13 00:55:44 +0900

    utageコマンド更新

--- commands/utage.js
+++ commands/utage.js
@@ -1,6 +1,6 @@
 exports.run = async (XPBot, message, args, level) => { // eslint-disable-line no-unused-vars
   message.delete();
-  message.channel.send('utage = Tokyo(福岡) = :muscle::seedling::fire:\r\n:congratulations:SimplePOSPool:muscle:対応')
+  message.channel.send('うたげ棒企画の発案者は実はumai\r\nI am シャイボーイ')
     .then(m => m.delete(15000))
     .catch(e => {
     if(e.code === 10008) XPBot.log('Utage', 'メッセージは既に削除されています: ' + e.path, 'ERR');
@@ -18,6 +18,6 @@ exports.conf = {
 exports.help = {
   name: "utage",
   category: "ネタ",
-  description: "utage is Tokyo(福岡) (1月17日現在)",
+  description: "utage (2月9日現在)",
   usage: "utage"
 };
