c506feb9724462905fcd4d01f3c852503cd140e5
MaySoMusician <maysomusician@gmail.com>
2018-02-13 01:01:28 +0900

    spamコマンド chatroomsオプション追加, 説明文更新

--- commands/spam.js
+++ commands/spam.js
@@ -2,7 +2,13 @@ const sendSpam = require('../modules/sendSpam.js');
 exports.run = async (XPBot, message, args, level) => { // eslint-disable-line no-unused-vars
   let guild = message.guild;
   
-  let channels = args.shift().split(';'); //args[0]
+  var channels;
+  if(args[0] == 'chatrooms') {
+    args.shift();
+    channels = ['chat_1_xp', 'chat_2_jk', 'chat_3_honobono', 'chat_4_markets', 'chat_5_gamers', 'chat_6_creator'];
+  }
+  else channels = args.shift().split(';');
+  //var channels = args.shift().split(';'); //args[0]
   let wait = parseInt(args.shift(), 10); //args[1]
   
   if(args.length == 0) return;
@@ -10,8 +16,6 @@ exports.run = async (XPBot, message, args, level) => { // eslint-disable-line no
   args[0] = args[0].replace('everyone', '@everyone');
   
   let spamMsg = args.join(' ');
-  
-  
   
   const settings = XPBot.getGuildSettings(message.guild);  
   
@@ -59,6 +63,6 @@ exports.conf = {
 exports.help = {
   name: "spam",
   category: "サーバー運営",
-  description: "指定されたチャンネルに一斉にメッセージを送信します",
+  description: "指定されたチャンネルに一斉にメッセージを送信します\nチャンネル名に「chatrooms」を指定すると、全チャットルームに送信\nメッセージ文頭に「everyone」を入力すると、自動的に@everyoneメンションに変わる",
   usage: "spam <チャンネル名(空白無し/先頭に「#」無し/「;」でつなげる)> <送信後書き込み停止時間(ミリ秒)> <メッセージ>"
 };
