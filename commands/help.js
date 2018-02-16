935460cb949eff39f51da4b2841ec066ce841d7b
MaySoMusician <maysomusician@gmail.com>
2018-02-16 23:18:21 +0900

    helpコマンドを特定権限許可に対応

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,7 @@ Helpコマンドはコマンド名と説明を表示するためのものです�
 */
 
 exports.run = (XPBot, message, args, level) => {
+  const levelName = XPBot.config.permLevels.find(l => l.level === level).name;
   // 引数無しの場合、全てのコマンドを一覧表示する
   if(!args[0]) {
     // サーバーごとの設定読み込み
@@ -12,7 +13,18 @@ exports.run = (XPBot, message, args, level) => {
     //const settings = message.guild ? XPBot.settings.get(message.guild.id) : XPBot.config.defaultSettings;
 
     // <Collection>.filter()関数で権限レベルでコマンドをフィルター
-    const myCommands = message.guild ? XPBot.commands.filter(cmd => XPBot.levelCache[cmd.conf.permLevel] <= level) : XPBot.commands.filter(cmd => XPBot.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
+    const myCommands = message.guild
+    ? XPBot.commands.filter(cmd => {
+      if(cmd.conf.specificAllowed) return cmd.conf.specificAllowed.includes(levelName);
+      else return XPBot.levelCache[cmd.conf.permLevel] <= level;
+    })
+    : XPBot.commands.filter(cmd => {
+      if(cmd.conf.guildOnly !== true){
+        if(cmd.conf.specificAllowed) return cmd.conf.specificAllowed.includes(levelName);
+        else return XPBot.levelCache[cmd.conf.permLevel] <= level;
+      }
+      return false;
+    });
 
     // 出力の整形のために最長コマンド名のみ抽出する
     const commandNames = myCommands.keyArray();
@@ -35,8 +47,17 @@ exports.run = (XPBot, message, args, level) => {
     let command = args[0];
     if(XPBot.commands.has(command)) {
       command = XPBot.commands.get(command);
-      if(level < XPBot.levelCache[command.conf.permLevel]) return;
-      message.channel.send(`= ${command.help.name} = \n${command.help.description}\n\n使用法　　:: ${command.help.usage}\n\nエイリアス:: ${command.conf.aliases.join(", ")}\n= ${command.help.name} =`, {code:"asciidoc"});
+      
+      if(command.conf.specificAllowed){
+        if(command.conf.specificAllowed.includes(levelName)){
+          message.channel.send(`= ${command.help.name} = \n\n${command.help.description}\n\n使用法　　:: ${command.help.usage}\n\nエイリアス:: ${command.conf.aliases.join(", ")}\n\n権限　　　:: ${command.conf.specificAllowed.join(' / ')}\n\n= ${command.help.name} =`, {code:"asciidoc"});
+        }
+      } else{
+        if(level > XPBot.levelCache[command.conf.permLevel]) {
+          message.channel.send(`= ${command.help.name} = \n\n${command.help.description}\n\n使用法　　:: ${command.help.usage}\n\nエイリアス:: ${command.conf.aliases.join(", ")}\n\n= ${command.help.name} =`, {code:"asciidoc"});
+        }
+      }
+      
     }
   }
 };
