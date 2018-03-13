029452c56a5026778cfb336aba4f5b6235c2f6aa
MaySoMusician <maysomusician@gmail.com>
2018-03-14 01:19:16 +0900

    radioSound.js 整理
    VC接続後の終了処理を抽出
    setVolをchangeVolとfadeに分離

--- modules/radioSound.js
+++ modules/radioSound.js
@@ -4,14 +4,28 @@ const ytdl = require('ytdl-core');
 var writeLog;
 
 module.exports = function(XPBot) {
+  let registerDisposingHandler = (gID, cnl) => {
+    XPBot.radioCenter.data[gID].disp.on('end', r => {
+      XPBot.radioCenter.dataReset(gID);
+      if(XPBot.radioCenter.data[gID].autonext) XPBot.radioCenter.ctrler.dequeue(guild);
+    });
+    XPBot.radioCenter.data[gID].virtualVol = vol;
+
+    let numDcn = cnc.listenerCount('disconnect');
+    if(numDcn < 1){
+      cnc.on('disconnect', ()=>{
+        if(XPBot.radioCenter.data[gID].disp) XPBot.radioCenter.data[gID].disp.end('切断');
+      });
+    }
+  };
+  
   let playYouTube = async ({guild: guild, cnl: cnl, movieID: movieID, opts: {seek = 0, vol = 0.01}, funcStart: funcStart}) => {
     let gID = guild.id.toString();
     
     if(typeof vol !== 'number') throw new TypeError('vol は数値でなければなりません');
 
     if(XPBot.radioCenter.data[gID].disp !== null){
-      await XPBot.radioCenter.ctrler.stop(guild);
-      //XPBot.radioCenter.data[gID].disp.end('他のYouTube動画の再生開始');
+      await XPBot.radioCenter.ctrler.stop(guild, '他のYouTube動画の再生開始');
     }
 
     cnl.join().then(async cnc => {
@@ -20,19 +34,8 @@ module.exports = function(XPBot) {
       await XPBot.wait(100);
       XPBot.radioCenter.data[gID].disp = cnc.playStream(stream, opts);
       if(typeof funcStart === 'function') XPBot.radioCenter.data[gID].disp.on('start', funcStart);
-      XPBot.radioCenter.data[gID].disp.on('end', r => {
-        //XPBot.radioCenter.data[gID] = XPBot.radioCenter.dataReset();
-        XPBot.radioCenter.dataReset(gID);
-        if(XPBot.radioCenter.data[gID].autonext) XPBot.radioCenter.ctrler.dequeue(guild);
-      });
-      XPBot.radioCenter.data[gID].virtualVol = vol;
       
-      let numDcn = cnc.listenerCount('disconnect');
-      if(numDcn < 1){
-        cnc.on('disconnect', ()=>{
-          if(XPBot.radioCenter.data[gID].disp) XPBot.radioCenter.data[gID].disp.end('切断');
-        });
-      }
+      registerDisposingHandler(gID, cnl);
     });
   };
   
@@ -42,28 +45,10 @@ module.exports = function(XPBot) {
     if(typeof vol !== 'number') throw new TypeError('vol は数値でなければなりません');
 
     if(XPBot.radioCenter.data[gID].disp !== null){
-      await XPBot.radioCenter.ctrler.stop(guild);
-      //XPBot.radioCenter.data[gID].disp.end('他の音楽ファイルの再生開始');
+      await XPBot.radioCenter.ctrler.stop(guild, '他の音楽ファイルの再生開始');
     }
     
-    cnl.join().then(async cnc => {
-      let opts = {seek: seek, volume: vol, passes: 3, bitrate: 'auto'};
-      XPBot.radioCenter.data[gID].disp = cnc.playFile('././assets/' + fileName, opts);
-      if(typeof funcStart === 'function') XPBot.radioCenter.data[gID].disp.on('start', funcStart);
-      XPBot.radioCenter.data[gID].disp.on('end', r => {
-        //XPBot.radioCenter.data[gID] = XPBot.radioCenter.dataReset();
-        XPBot.radioCenter.dataReset(gID);
-        if(XPBot.radioCenter.data[gID].autonext) XPBot.radioCenter.ctrler.dequeue(guild);
-      });
-      XPBot.radioCenter.data[gID].virtualVol = vol;
-
-      let numDcn = cnc.listenerCount('disconnect');
-      if(numDcn < 1){
-        cnc.on('disconnect', ()=>{
-          if(XPBot.radioCenter.data[gID].disp) XPBot.radioCenter.data[gID].disp.end('切断');
-        });
-      }
-    });
+    registerDisposingHandler(gID, cnl);
   };
   
   let playFileAlias = ({guild: guild, cnl: cnl, alias: alias, opts: {seek: seek = 0, vol: vol = 0.01}, funcStart: funcStart}) => {
@@ -108,15 +93,15 @@ module.exports = function(XPBot) {
     XPBot.radioCenter.data[gID].autonext = value;
   };
 
-  let stop = async (guild) => {
+  let stop = async (guild, reason = '再生停止') => {
     let gID = guild.id.toString();
-    await XPBot.radioCenter.ctrler.setVol(guild, 0, true, 2000);
-    XPBot.radioCenter.data[gID].disp.end('再生停止');
+    //await XPBot.radioCenter.ctrler.setVol(guild, 0, true, 2000);
+    XPBot.radioCenter.data[gID].disp.end(reason);
   };
 
   let pause = async (guild) => {
     let gID = guild.id.toString();
-    await XPBot.radioCenter.ctrler.setVol(guild, 0, true, 1000, true);
+    await XPBot.radioCenter.ctrler.fade(guild, 0, true, 1000, true);
     XPBot.radioCenter.data[gID].disp.pause();
   };
 
@@ -124,41 +109,46 @@ module.exports = function(XPBot) {
     let gID = guild.id.toString();
     let vol = XPBot.radioCenter.data[gID].virtualVol;
     XPBot.radioCenter.data[gID].disp.resume();
-    XPBot.radioCenter.ctrler.setVol(guild, 0, false, null, true);
+    XPBot.radioCenter.ctrler.changeVol(guild, 0, false, null, true);
     await XPBot.wait(500);
-    await XPBot.radioCenter.ctrler.setVol(guild, vol, true, 1500, true);
+    await XPBot.radioCenter.ctrler.fade(guild, vol, true, 1500, true);
   }
 
-  let setVol = (guild, vol, fade, fadeSpan, prevVirtualVolChange) => {
+  let changeVol = (guild, vol, prevVirtualVolChange) => {
     let gID = guild.id.toString();
     if(typeof vol !== 'number') throw new TypeError('vol は数値でなければなりません');
     
-    if(vol > 2) vol = 2;
+    //if(vol > 2) vol = 2;
+    if(vol === 0) vol = 0.01;
     
     if(!prevVirtualVolChange) XPBot.radioCenter.data[gID].virtualVol = vol;
+    
+    XPBot.radioCenter.data[gID].disp.setVolume(vol);
+    return true; 
+  }
+  
+  let fade = (guild, vol, fade, fadeSpan, prevVirtualVolChange) => {
+    let gID = guild.id.toString();
+    if(typeof vol !== 'number') throw new TypeError('vol は数値でなければなりません');
 
-    if(fade){
-      let start = XPBot.radioCenter.data[gID].disp.volume,
-          times = fadeSpan / 50,
-          diff = vol - start,
-          step = diff / times,
-          fVol = start;
-      return new Promise(async (resolve, reject) => {
-        for(let i = 0; i < times; i++){
-          fVol += step;
-          XPBot.radioCenter.data[gID].disp.setVolume(fVol);
-          //console.log(fVol);
-          await XPBot.wait(50);
-        }
-        XPBot.radioCenter.data[gID].disp.setVolume(vol);
-        resolve();
-      });
+    //if(vol > 2) vol = 2;
 
-    }else{
-      if(vol === 0) vol = 0.01;
+    if(!prevVirtualVolChange) XPBot.radioCenter.data[gID].virtualVol = vol;
+
+    let start = XPBot.radioCenter.data[gID].disp.volume,
+        times = fadeSpan / 50,
+        diff = vol - start,
+        step = diff / times,
+        fVol = start;
+    return new Promise(async (resolve, reject) => {
+      for(let i = 0; i < times; i++){
+        fVol += step;
+        XPBot.radioCenter.data[gID].disp.setVolume(fVol);
+        await XPBot.wait(50);
+      }
       XPBot.radioCenter.data[gID].disp.setVolume(vol);
-      return true;
-    }    
+      resolve();
+    });
   }
   
   let forceReset = (guild) => {
@@ -166,20 +156,6 @@ module.exports = function(XPBot) {
       return c.channel.guild.id === guild.id
     }).disconnect();
   }
-
-  /*let fadeOut = async (guild, time) => {
-    let gID = guild.id.toString();
-    let start = XPBot.radioCenter.data[gID].disp.volume,
-        loop = time / 50,
-        diff = start / loop;
-
-    for(let foVol = start; foVol >= 0; foVol -= diff){
-      XPBot.radioCenter.data[gID].disp.setVolume(foVol);
-      console.log(foVol);
-      await XPBot.wait(50);
-    }
-    XPBot.radioCenter.data[gID].disp.end('再生停止');
-  }*/
   
   let oldCtrler = XPBot.radioCenter.ctrler;
   
@@ -193,7 +169,8 @@ module.exports = function(XPBot) {
     stop: stop,
     pause: pause,
     resume: resume,
-    setVol: setVol,
+    changeVol: changeVol,
+    fade: fade,
     forceReset: forceReset
   };
   
